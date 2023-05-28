660822097ffe093d9bc5e495e5dd88f03a48f5e7
Hqputu <Hqputu@yahoo.com>
2023-05-27 22:40:22 -0500

    test created

---
+++ Ladyswork.java
@@ -0,0 +1,6 @@
+public class Ladyswork {
+    public static void main(String[] args) {
+        System.out.println("Hello how are you today");
+        System.out.println("i hope to enjoy this section");
+    }
+}
